bc71162c8cffed7727d77d62f4bb114b7232c3a9
Heather Laude <hlaude@ufl.edu>
2015-03-21 13:20:07 -0400

    change formatting of p elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function() {
     var paragraphs = $('p');
+    paragraphs.css({
+        "background-color": "#4c8aa9",
+        "color": "white",
+        "font-family": "calibri, trebuchet ms"
+    });
 });
